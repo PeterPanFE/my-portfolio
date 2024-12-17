ef8db9031207263c63edb534b34f58298ec5ecb9
Pedro Afonso Oliveira <pedro.afonso.oliveira@polarising.com>
2024-12-17 12:47:50 +0000

    mobile stickyButtons fix

--- src/components/StickyButtons/constants.js
+++ src/components/StickyButtons/constants.js
@@ -3,5 +3,5 @@ export const STICKY_BUTTONS_GROUP = "group relative flex items-center";
 export const STICKY_BUTTONS_BUTTON = "w-20 h-20 flex items-center justify-center bg-gradient-to-b from-violet-600 to-violet-800 rounded-full shadow-2xl transform transition-all duration-300 ease-in-out group-hover:translate-x-[-9rem] z-10";
 export const STICKY_BUTTONS_LABEL = "absolute left-full ml-2 h-20 flex items-center justify-center min-w-max px-4 py-2 pl-[55px] bg-violet-500 text-white text-sm font-medium rounded-md opacity-0 transform translate-x-full group-hover:opacity-100 group-hover:translate-x-[-100%] transition-all duration-300 ease-in-out z-0 cursor-pointer font-bold";
 
-export const STICKY_BUTTONS_MOBILE_WRAPPER = "fixed bottom-0 left-0 w-full bg-white flex justify-around py-4 z-50 border-t border-gray-200";
-export const STICKY_BUTTONS_MOBILE_GROUP = "flex flex-col items-center text-center text-sm font-medium text-gray-700";
+export const STICKY_BUTTONS_MOBILE_WRAPPER = "fixed bottom-0 left-0 w-full bg-gradient-to-b from-violet-600 to-violet-800 flex justify-around z-50";
+export const STICKY_BUTTONS_MOBILE_GROUP = "flex flex-col w-[50%] h-[100%] py-4 items-center text-center text-sm font-medium text-white border border-violet-500";
