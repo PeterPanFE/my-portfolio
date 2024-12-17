d6d6876fceac13eb5338e009283e44672fad1a51
Pedro Afonso Oliveira <pedro.afonso.oliveira@polarising.com>
2024-12-17 15:33:02 +0000

    fix margin bottom

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className="relative z-0 sm:mb-[100px]">
+        <div className="relative z-0 sm:mb-[100px] md:mb-0 ">
           <Contact />
           <StarsCanvas />
         </div>
